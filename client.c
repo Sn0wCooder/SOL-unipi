f4bc3aed680a50ecb0bfb453140cd01f074bb4d2
Leonardo Alfreducci <leonardo@MacBook-Pro-di-Leonardo.local>
2021-06-27 11:49:32 +0200

    client connessione al server, inizio esecuzione comandi dalla coda

--- client.c
+++ client.c
@@ -22,6 +22,15 @@
 #define SOCKNAME "ProvaSock.sk"
 long sockfd;
 
+static int add_to_current_time(long sec, long nsec, struct timespec* res){
+    // TODO: maybe check its result
+    clock_gettime(CLOCK_REALTIME, res);
+    res->tv_sec += sec;
+    res->tv_nsec += nsec;
+
+    return 0;
+}
+
 
 int set_timespec_from_msec(long msec, struct timespec* req) {
   if(msec < 0 || req == NULL){
@@ -50,26 +59,16 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
 
 
     struct sockaddr_un serv_addr;
-    int sockfd;
+    //int sockfd;
     SYSCALL_EXIT("socket", sockfd, socket(AF_UNIX, SOCK_STREAM, 0), "socket", "");
     memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sun_family = AF_UNIX;
     strncpy(serv_addr.sun_path,SOCKNAME, strlen(SOCKNAME)+1);
 
-    int notused;
-    SYSCALL_EXIT("connect", notused, connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)), "connect", "");
-
-
-
-
-
-
-
-
-
-
-    /*// setting waiting time
+    /*int notused;
+    SYSCALL_EXIT("connect", notused, connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)), "connect", "");*/
+    // setting waiting time
     struct timespec wait_time;
     // no need to check because msec > 0 and &wait_time != NULL
     set_timespec_from_msec(msec, &wait_time);
@@ -79,13 +78,14 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
     clock_gettime(CLOCK_REALTIME, &curr_time);
 
     //fprintf(stderr, "fin qui");
-    sleep(3);
+    //sleep(3);
     // trying to connect
     int err = -1;
+    fprintf(stderr, "currtime %ld abstime %ld\n", curr_time.tv_sec, abstime.tv_sec);
     while( (err = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) == -1
             && curr_time.tv_sec < abstime.tv_sec ){
         //debug("connect didn't succeed, trying again...\n");
-        fprintf(stderr, "err4\n");
+        fprintf(stderr, "err4 %d\n", errno);
 
         if( nanosleep(&wait_time, NULL) == -1){
             sockfd = -1;
@@ -103,21 +103,46 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
         sockfd = -1;
         errno = ETIMEDOUT;
         return -1;
-    }*/
+    }
 
     //debug("Connected! :D\n");
 
     //socket_path = sockname;
+    fprintf(stderr, "mi sono connesso al server!\n");
     return 0;
 }
 
+int EseguiComandoClient(NodoComando *tmp) {
+  int notused;
+  char *buffer=NULL;
+  if(tmp == NULL) return -1; //errore: tmp non può e non deve essere NULL. Abbiamo già controllato che q->len > 0
+  char* towrite = malloc(sizeof(char) * (strlen(tmp->name) + 1)); //alloco la stringa da scrivere, che sarà del tipo "rfile"
+  towrite[0] = tmp->cmd;
+  for(int i = 1; i <= strlen(tmp->name); i++)
+    towrite[i] = tmp->name[i - 1];
+  fprintf(stderr, "sto scrivendo nel socket %s\n", towrite);
+  int n = strlen(towrite) + 1;
+
+  SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", "");
+  SYSCALL_EXIT("writen", notused, writen(sockfd, towrite, n*sizeof(char)), "write", "");
+
+  buffer = realloc(buffer, n*sizeof(char));
+  if (!buffer) { perror("realloc"); fprintf(stderr, "Memoria esaurita....\n"); }
+
+
+  SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read","");
+  SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n*sizeof(char)), "read","");
+  buffer[n] = '\0';
+  printf("result: %s\n", buffer);
+}
+
 int main(int argc, char *argv[]) {
-  //Queue *q = parser(argc, argv); //coda delle operazioni
-  //struct timespec abstime;
+  Queue *q = parser(argc, argv); //coda delle operazioni
+  struct timespec abstime;
 
-  //add_to_current_time(config.waiting_sec, 0, &abstime);
+  add_to_current_time(2, 0, &abstime);
 
-  struct sockaddr_un serv_addr;
+  /*struct sockaddr_un serv_addr;
   int sockfd;
   SYSCALL_EXIT("socket", sockfd, socket(AF_UNIX, SOCK_STREAM, 0), "socket", "");
   memset(&serv_addr, '0', sizeof(serv_addr));
@@ -126,12 +151,16 @@ int main(int argc, char *argv[]) {
   strncpy(serv_addr.sun_path,SOCKNAME, strlen(SOCKNAME)+1);
 
   int notused;
-  SYSCALL_EXIT("connect", notused, connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)), "connect", "");
-  //int x = openConnection(SOCKNAME, 0, abstime); //da vedere se da errore
-  //fprintf(stderr, "fdsock: %ld, risultato %d\n", sockfd, x);
-  //while(q->len > 0) { //finchè ci sono richieste che il parser ha visto
+  SYSCALL_EXIT("connect", notused, connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)), "connect", "");*/
+  int x = openConnection(SOCKNAME, 0, abstime); //da vedere se da errore
+  fprintf(stderr, "sockfd: %ld, risultato %d\n", sockfd, x);
 
-  //}
+
+  while(q->len > 0) { //finchè ci sono richieste che il parser ha visto
+    NodoComando *tmp = pop(&q);
+    EseguiComandoClient(tmp);
+
+  }
 
   return 0;
 }
